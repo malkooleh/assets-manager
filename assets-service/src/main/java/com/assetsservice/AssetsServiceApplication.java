ac6f344a8adcf91935e1941d1788396ee12b6076
malkooleh <maluy709@gmail.com>
2025-04-27 17:10:14 +0300

    add tracing

--- assets-service/src/main/java/com/assetsservice/AssetsServiceApplication.java
+++ assets-service/src/main/java/com/assetsservice/AssetsServiceApplication.java
@@ -1,15 +1,9 @@
 package com.assetsservice;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -18,13 +12,5 @@ public class AssetsServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AssetsServiceApplication.class, args);
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper()
-                .registerModule(new JavaTimeModule())
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .disable(WRITE_DATES_AS_TIMESTAMPS);
     }
 }
